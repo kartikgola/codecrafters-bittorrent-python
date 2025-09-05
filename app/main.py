29383739ae98385fecf1db5ba636755d3364d47c
Kartik Gola <krtkgla@gmail.com>
2025-09-05 15:52:38 +0000

    [any message]

--- app/main.py
+++ app/main.py
@@ -42,8 +42,7 @@ def main():
             raise TypeError(f"Type not serializable: {type(data)}")
 
         # Uncomment this block to pass the first stage
-        # print(json.dumps(decode_bencode(bencoded_value), default=bytes_to_str))
-        print(decode_bencode(bencoded_value).decode())
+        print(json.dumps(decode_bencode(bencoded_value), default=bytes_to_str))
     else:
         raise NotImplementedError(f"Unknown command {command}")
 
